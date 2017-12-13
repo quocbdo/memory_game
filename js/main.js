71ebe9b69dc76e3e6094c458dc8ad63b484b4252
Quoc Do <quocbdo@gmail.com>
2017-12-12 18:08:55 -0800

    Created game logic

--- js/main.js
+++ js/main.js
@@ -1,20 +1,44 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+  {
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png",
+  },
+  {
+    rank: "queen",
+    suit: "diamonds",
+    cardImage:"images/queen-of-diamonds.png",
+  },
+  {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png",
+  },
+  {
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png",
+  }
+];
 
 var cardsInPlay = [];
 
 var checkForMatch = function() {
-  if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!");
-  } else {
-    alert("Sorry, try again.");
+  if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+      alert("You found a match!");
+    } else {
+      alert("Sorry, try again.");
+    }
   }
 }
 
 var flipCard = function(cardId) {
-  if (cardsInPlay.length === 2) {
-    checkForMatch();
-  }
-  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId].rank);
+  console.log("User flipped " + cards[cardId].rank);
+  console.log(cards[cardId].cardImage);
+  console.log(cards[cardId].suit);
+  checkForMatch();
 }
 
 flipCard(0);
